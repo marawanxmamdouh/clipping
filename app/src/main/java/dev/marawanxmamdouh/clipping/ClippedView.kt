4754065215b3081ea897bbb4415bab204fa8a551
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-20 03:16:10 +0200

    Implement quick Reject

--- app/src/main/java/dev/marawanxmamdouh/clipping/ClippedView.kt
+++ app/src/main/java/dev/marawanxmamdouh/clipping/ClippedView.kt
@@ -49,6 +49,8 @@ class ClippedView @JvmOverloads constructor(
         clipRectBottom - rectInset
     )
 
+    private val rejectRow = rowFour + rectInset + 2 * clipRectBottom
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         drawBackAndUnclippedRectangle(canvas)
@@ -270,5 +272,39 @@ class ClippedView @JvmOverloads constructor(
     }
 
     private fun drawQuickRejectExample(canvas: Canvas) {
+        val inClipRectangle = RectF(
+            clipRectRight / 2,
+            clipRectBottom / 2,
+            clipRectRight * 2,
+            clipRectBottom * 2
+        )
+
+        val notInClipRectangle = RectF(
+            RectF(
+                clipRectRight + 1,
+                clipRectBottom + 1,
+                clipRectRight * 2,
+                clipRectBottom * 2
+            )
+        )
+
+        canvas.save()
+        canvas.translate(columnOne, rejectRow)
+        canvas.clipRect(
+            clipRectLeft, clipRectTop,
+            clipRectRight, clipRectBottom
+        )
+        if (canvas.quickReject(
+                inClipRectangle, Canvas.EdgeType.AA
+            )
+        ) {
+            canvas.drawColor(Color.WHITE)
+        } else {
+            canvas.drawColor(Color.BLACK)
+            canvas.drawRect(
+                inClipRectangle, paint
+            )
+        }
+        canvas.restore()
     }
 }
